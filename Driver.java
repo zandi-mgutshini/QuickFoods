8e18a8f497a18a2602d6aec30b0f3a706170261d
Zandile Mgutshini <zmgutshini@icloud.com>
2023-06-14 18:47:28 +0200

    Created driver class

---
+++ Driver.java
@@ -0,0 +1,41 @@
+public class Driver {
+    private String driverName;
+    private String driverLocation;
+    private int driverLoad;
+
+    public Driver(String driverName, String driverLocation, int driverLoad) {
+        this.driverName = driverName;
+        this.driverLocation = driverLocation;
+        this.driverLoad = driverLoad;
+    }
+
+    public Driver() {
+        this.driverName = "Generic Driver";
+        this.driverLocation = "Unkown";
+        this.driverLoad = 0;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getDriverLocation() {
+        return driverLocation;
+    }
+
+    public void setDriverLocation(String driverLocation) {
+        this.driverLocation = driverLocation;
+    }
+
+    public int getDriverLoad() {
+        return driverLoad;
+    }
+
+    public void setDriverLoad(int driverLoad) {
+        this.driverLoad = driverLoad;
+    }
+}
